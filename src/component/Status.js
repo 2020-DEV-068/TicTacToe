ad9abcafd9c8154a3b8f2c9a6cd0d6292794fe79
2020-DEV-068 <2020dev068@gmail.com>
2020-05-13 15:47:20 +0530

    refactor: Extract in to method for redability

--- src/component/Status.js
+++ src/component/Status.js
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types';
 import Constants from '../constants/Constants';
 
 const Status = (props) => {
+    const { currentPlayer, board } = props;
+
     const updateStatus = () => {
-        const { currentPlayer, board } = props;
-
-        if (Constants.FIRST_ROW_TILES.map((tile) => board[tile])
-            .every((value, index, arr) => value && value === arr[Constants.INITIAL_TILE_POSITION])) {
-
+        if (isFirstRowCompletedByAPlayer()) {
             return Constants.STATUS_WINNER
                 + board[Constants.FIRST_ROW_TILES[Constants.INITIAL_TILE_POSITION]];
         }
 
         return Constants.CURRENT_PLAYER + currentPlayer;
+    };
+
+    const isFirstRowCompletedByAPlayer = () => {
+        return Constants.FIRST_ROW_TILES.map((tile) => board[tile])
+            .every((value, index, arr) => value && value === arr[Constants.INITIAL_TILE_POSITION]);
     };
 
     return (
