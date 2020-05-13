7d80254d28f7811620e3bb8a5256ecff9a8c2c08
2020-DEV-068 <2020dev068@gmail.com>
2020-05-13 11:59:08 +0530

    feat: Display game board with 9 empty tiles

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import '../App.css';
+import Tile from './Tile';
 import StyleConstants from '../constants/StyleConstants';
 import Constants from '../constants/Constants';
 
 const Game = () => {
+    const renderBoard = () => {
+        let tiles = [];
+        for (let tile = Constants.INITIAL_TILE_POSITION; tile < Constants.MAXIMUM_NUMBER_OF_TILES; tile++) {
+            tiles.push(
+                <Tile key={tile} />
+            );
+        }
+        return tiles;
+    };
+
     return (
         <div className={StyleConstants.APP}>
             <header className={StyleConstants.APP_HEADER}>
                 {Constants.APP_TITLE}
             </header>
+            <div>
+                <ul className={StyleConstants.BOARD}>
+                    {renderBoard()}
+                </ul>
+            </div>
         </div>
     );
 }
